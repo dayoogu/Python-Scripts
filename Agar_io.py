896e6593b770b2c5065b925ef9a20d37d1cb442d
Pythonistapro777 <dayoogu@gmail.com>
2015-10-04 19:08:40 +0300

    Update Agar_io.py

--- Agar_io.py
+++ Agar_io.py
@@ -1,23 +1,23 @@
-#By: Adedayo Ogunnoiki ft [cclauss]
+#By: Adedayo Ogunnoiki
 from scene import *
 from random import *
 import sys
-import console
 
 class Particle(Rect):
         def __init__(self, wh):
-                self.w = wh.w
-                self.h = wh.h
-                self.x = randint(self.w*-1, self.w)
-                self.y = randint(self.h*-1, self.h)
+                global particlew, particleh
+                particlew = wh.w
+                particleh = wh.h
+                self.x = randint(particlew*-1, particlew)
+                self.y = randint(particleh*-1, particleh)
                 self.colour = Color(random(), random(), random())
-                self.cells=Rect(self.x, self.y, 5, 5)
+                self.cells=Rect(self.x, self.y, 10, 10)
 
         def update(self):
-                self.cells=Rect(self.x, self.y, 5, 5)
+                self.cells=Rect(self.x, self.y, 10, 10)
 
         def draw(self):
-                stroke(*self.colour)
+                fill(*self.colour)
                 ellipse(*self.cells)
                 global attackx, attacky
                 attackx, attacky = self.x, self.y
@@ -29,118 +29,138 @@ class Bots(Rect):
                 self.x = randint(self.w*-1, self.w)
                 self.y = randint(self.h*-1, self.h)
                 self.colour = Color(random(), random(), random())
-                self.bsize=8
-
-                self.lw=self.w*-1
-                self.rw=self.w
-                self.bh=self.h*-1
-                self.th=self.h
+                self.bsize=10
 
         def update(self):
                 self.attackers=Rect(self.x, self.y, self.bsize, self.bsize)
-                stroke(0,0,0)
-                stroke_weight(4)
-                line(self.lw, self.bh, self.lw, self.th)
-                line(self.lw, self.th, self.rw, self.th)
-                line(self.rw, self.bh, self.rw, self.th)
-                line(self.lw, self.bh, self.rw, self.bh)
 
         def draw(self):
                 fill(*self.colour)
-                stroke(*self.colour)
                 ellipse(*self.attackers)
 
 class Intro(Scene):
         def setup(self):
-                global count, psize
-                count = 0
-                psize = 8
-                plocx = 240
-                plocy = 160
-                self.player = Rect(plocx, plocy, psize, psize)
+                self.count = 0
+                self.psize = 10
+                self.plocx = self.size.w/2 - self.psize/2
+                self.plocy = self.size.h/2 - self.psize/2
+                self.player = Rect(self.plocx, self.plocy, self.psize, self.psize)
                 self.colour = Color(random(), random(), random())
                 self.particles = [Particle(self.size) for i in xrange(50)]
                 self.bots = [Bots(self.size)]
+                for p in self.particles:
+                        self.lw=self.size.w*-1
+                        self.rw=self.size.w
+                        self.bh=self.size.h*-1
+                        self.th=self.size.h
+
+                self.split = Button(Rect(self.size.w/8, self.size.h/16, 80, 80))
+                self.split.background = Color(0,0,0,0)
+                self.split.stroke = Color(0,0,0,0)
+                self.split.image = 'ionicons-code-working-24'
+                self.split.action = self.split_player
+                self.add_layer(self.split)
+
+        def split_player(self):
+                if self.psize/2 > 10:
+                        print("yas, bitch yas")
 
         def touch_began(self, touch):
                 global x1, y1
                 x1, y1 = touch.location
 
-
         def touch_moved(self, touch):
-                global x, y, count
+                global x, y
                 x, y = touch.location
-                count = 1
+                self.count = 1
 
         def movecells(self):
-                global x, y, x1, y1, plocx, plocy, psize
+                global x, y, x1, y1, particlew, particleh
                 for p in self.particles:
+                        if x1 > x and self.lw < self.plocx:
+                                p.x += 2
+                                self.lw += 0.04
+                                self.rw += 0.04
+                                particlew += 0.04
+
+                        if x1 < x and self.rw > self.plocx:
+                                p.x -= 2
+                                self.lw -=0.04
+                                self.rw -=0.04
+                                particlew -=0.04
+
+                        if y1 > y and self.bh < self.plocy:
+                                p.y += 2
+                                self.bh += 0.04
+                                self.th += 0.04
+                                particleh += 0.04
+
+                        if y1 < y and self.th > self.plocy:
+                                p.y -= 2
+                                self.bh -=0.04
+                                self.th -=0.04
+                                particleh -=0.04
+
                         for b in self.bots:
-                                if x1 > x and b.lw < plocx:
-                                        p.x += 2
-                                        b.lw += 0.04
-                                        b.rw += 0.04
-                                if x1 < x and b.rw > plocx+psize:
-                                        p.x += -2
-                                        b.lw += -0.04
-                                        b.rw += -0.04
-                                if y1 > y and b.bh < plocy:
-                                        p.y += 2
-                                        b.bh += 0.04
-                                        b.th += 0.04
-                                if y1 < y and b.th > plocy+psize:
-                                        p.y += -2
-                                        b.bh += -0.04
-                                        b.th += -0.04
+                                if b.x > self.rw:
+                                        b.x -=1
+                                elif b.x < self.lw:
+                                        b.x += 1
+                                if b.y < self.bh:
+                                        b.y += 1
+                                elif b.y > self.th:
+                                        b.y -=1
 
         def die(self):
                 sys.exit()
-                console.clear()
 
         def draw(self):
-                global attackx, attackx, count, plocx, plocy, psize
-                if count == 1:
-                        self.movecells()
                 background(0.00, 0.05, 0.20)
+                self.split.background = Color(0,0,0,0)
+                self.split.draw()
+                global attackx, attackx
+                self.plocx = self.size.w/2 - self.psize/2
+                self.plocy = self.size.h/2 - self.psize/2
+                if self.count == 1:
+                        self.movecells()
                 for p in self.particles:
                         p.update()
                         p.draw()
-                        plocx = p.w/2 - psize/2
-                        plocy = p.h/2 - psize/2
                         if self.player.intersects(p.cells):
                                 self.particles.remove(p)
-                                psize += 0.2
+                                self.psize += 0.2
                                 for p in range(1):
                                         self.particles.append(Particle(self.size))
                 for b in self.bots:
                         b.update()
                         b.draw()
                         for p in self.particles:
-                                if b.x < attackx:
+                                if b.x < attackx and b.x < self.rw:
                                         b.x += 0.05
-                                elif b.x > attackx:
+                                elif b.x > attackx and b.x > self.lw:
                                         b.x -= 0.05
-                                if b.y < attacky:
+                                if b.y < attacky and b.y < self.th:
                                         b.y += 0.05
-                                elif b.y > attacky:
+                                elif b.y > attacky and b.y > self.bh:
                                         b.y -= 0.05
 
                                 if b.attackers.intersects(p.cells):
                                         self.particles.remove(p)
-                                        b.bsize+=0.2
+                                        b.bsize+=0.5
                                         for p in range(1):
                                                 self.particles.append(Particle(self.size))
 
                                 if self.player.intersects(b.attackers):
-                                        if b.bsize > psize:
+                                        if b.bsize > self.psize:
                                                 self.die()
-                                        if psize > b.bsize:
-                                                psize += b.bsize
+                                        if self.psize > b.bsize:
+                                                self.psize += b.bsize
 
-
-                self.player = Rect(plocx, plocy, psize, psize)
-                stroke(*self.colour)
+                self.player = Rect(self.plocx, self.plocy, self.psize, self.psize)
                 fill(*self.colour)
                 ellipse(*self.player)
 
-run(Intro(), LANDSCAPE)
+        def should_rotate(self, orientation):
+                return True
+
+run(Intro())
